f5dec09ff301fe6ab138ca4fea26f6cb8dc65d93
Shadir Amjard <mshadir287@gmail.com>
2026-06-16 18:10:12 +0530

    Implement job listing view and update routing for jobs index

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\JobController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/show', function () {
+Route::get('/jobs.index', function () {
     $job = Job::first();
 
     if (! $job) {
         abort(404, 'No jobs available.');
     }
 
-    return redirect()->route('jobs.show', $job);
+    return redirect()->route('jobs.index', $job);
 });
 
 Route::resource('jobs', JobController::class);
