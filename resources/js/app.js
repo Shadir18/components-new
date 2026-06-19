f13f3fd02c95d8aa26bf9bb95a3a2ad4bdb9d19e
Shadir Amjard <mshadir287@gmail.com>
2026-06-19 09:26:17 +0530

    TEST COMMIT

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,7 @@ window.$ = $;
 window.jQuery = $;
 window.axios = axios;
 
-// Set default CSRF token for all axios requests
 axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]')?.content;
 
 import 'popper.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js'; 
