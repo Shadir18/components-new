12c9465efafa3c58be145cbcafa54965ccc663e0
Shadir18 <mshadir287@gmail.com>
2026-06-26 03:02:33 +0530

    created a route to display the products page on http://127.0.0.1:8000/ This url

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function () {
     Route::resource('products', ProductController::class);
 });
+Route::get('/', [ProductController::class, 'index']);
 Route::resource('products', ProductController::class)->only(['index', 'show']);
 
 Route::get('/about', function(){
